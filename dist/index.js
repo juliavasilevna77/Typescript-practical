b33fff73b62c36cd81ce2c909b86d8e5cd67d8cd
Julia <119414779+juliavasilevna77@users.noreply.github.com>
2025-11-30 00:19:26 +0200

    Додано JavaScript-код у dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,10 @@
+const userName = "Юлія";
+const age = 28;
+const isActive = true;
 
+function printUserInfo(name, age, active) {
+  const status = active ? "активна" : "неактивна";
+  console.log(`Користувач: ${name}, вік: ${age}, статус: ${status}`);
+}
+
+printUserInfo(userName, age, isActive);
